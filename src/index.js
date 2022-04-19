9f73764f5000afe3e516fd98e6b65030c2712cb5
Matt O'Donnell <matthewtwelve28@gmail.com>
2022-04-19 18:27:51 -0400

    #1 check notes for conclusions

--- src/index.js
+++ src/index.js
@@ -69,6 +69,7 @@ class Game extends React.Component {
     this.state = {
       history: [{
         squares: Array(9).fill(null),
+        order: Array(9).fill(null)
       }],
       stepNumber: 0,
       xIsNext: true,
@@ -79,6 +80,7 @@ class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
+    const currOrder = current.order.slice();
 
 
     if (calculateWinner(squares) || squares[i]) {
@@ -86,9 +88,11 @@ class Game extends React.Component {
     }
 
     squares[i] = this.state.xIsNext ? 'X' : 'O';
+    currOrder[this.state.stepNumber] = i;
     this.setState({
       history: history.concat([{
         squares: squares,
+        order: currOrder
       }]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
@@ -102,15 +106,47 @@ class Game extends React.Component {
     });
   }
 
+  compare(move) {
+    const history = this.state.history;
+    const curr = history[move];
+    const last = history[move - 1];
+
+    for(var i = 0; i < 9; i++) {
+      if(curr.squares[i] !== last.squares[i]){
+        return i;
+      }
+    }
+  }
+
+  getCol(move) {
+    if (move === 0) {
+      return;
+    } else {
+      const index = this.compare(move);
+      return index % 3 + 1;
+    }
+  }
+
+  getRow(move) {
+    if (move === 0) {
+      return;
+    } else {
+      const index = this.compare(move);
+      return Math.floor(index / 3) + 1;
+    }
+  }
+
 
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
+    
+
     const moves = history.map((step, move) => {
       const desc = move ?
-        'Go to move #' + move :
+        'Go to move #' + move + " (" + this.getCol(move) + "," + this.getRow(move) + ")" :
         'Go to game start';
       return (
         <li key={move}>
